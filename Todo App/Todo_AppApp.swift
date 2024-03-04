fad50858840b072a56e8765b450a99056e4bcc1d
Sivarajan <citysiva180@users.noreply.github.com>
2024-03-04 07:29:17 +0530

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Sivarajan on 04/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
